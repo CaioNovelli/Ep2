05cd88c96c631ec4fc981a4c597e154d9ddbef35
RicardoYuh <130596497+RicardoYuh@users.noreply.github.com>
2023-07-27 12:15:14 -0300

    commit ex5

---
+++ soteia_questao_inedita.py
@@ -0,0 +1,14 @@
+import random
+def sorteia_questao (questao,opcao):
+    if opcao in questao:
+        questoes_1 = questao[opcao]
+        dado = random.randint(0,len(questoes_1)-1)       
+        return questoes_1[dado]
+    else:
+        return None    
+def sorteia_questao_inedita(quest, nivel, quest_soteada):
+    questao = sorteia_questao(quest, nivel)
+    while questao in quest_soteada:
+        questao = sorteia_questao(quest, nivel)
+    quest_soteada.append(questao)
+    return questao
